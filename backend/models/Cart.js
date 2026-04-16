b7f83bffed0a3b2e050970c766ce9235e092e2f4
KABILAN K <148975907+Kabi09@users.noreply.github.com>
2026-04-16 13:40:13 +0530

    update Cart.js

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const cartSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
   items: [{
